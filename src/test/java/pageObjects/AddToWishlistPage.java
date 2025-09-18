28ddd8357920b87efdd7a9ac045316b1d77b865a
Kaviya0624 <kaviyamalya3000@gmail.com>
2025-09-18 18:56:18 +0530

    added wishlisttestcase

---
+++ src/test/java/pageObjects/AddToWishlistPage.java
@@ -0,0 +1,111 @@
+package pageObjects;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class AddToWishlistPage extends BasePage {
+	
+	public AddToWishlistPage(WebDriver driver)
+	{
+		super(driver);
+	}
+
+	@FindBy(xpath="//a[normalize-space()='Cameras']")
+	WebElement Camera;
+	
+	@FindBy(css ="div[id='content'] div:nth-child(1) div:nth-child(1) div:nth-child(2) div:nth-child(2) button:nth-child(2) i:nth-child(1)")
+	WebElement AddtoWish;
+	
+	@FindBy(xpath="//a[normalize-space()='Nikon D300']")
+	WebElement NikonCamera;
+	
+	@FindBy(xpath="//div[@id='product-product']//div[@class='btn-group']//button[1]")
+	WebElement AddtoWish2;
+	
+	@FindBy(xpath="//a[normalize-space()='Tablets (1)']")
+	WebElement Tablets;
+	
+	@FindBy(xpath="//button[@type='button']//i[@class='fa fa-heart']")
+	WebElement AddtoWish3;
+	
+	@FindBy(xpath="//i[@class='fa fa-heart']")
+	WebElement Wishlistmenu;
+	
+	@FindBy(xpath="//h2[normalize-space()='My Wish List']")
+	WebElement txtConfirm;
+	
+	@FindBy(xpath="//div[@class='alert alert-success alert-dismissible']")
+	WebElement alert;
+	
+	@FindBy(xpath="//a[normalize-space()='Continue']")
+	WebElement contin;
+	
+	@FindBy(xpath="//a[@class='list-group-item'][normalize-space()='Wish List']")
+	WebElement AccoWL;
+	
+	public void clickCamera()
+	{
+		Camera.click();
+	}
+	
+	public void clickAddtoWish()
+	{
+		AddtoWish.click();
+	}
+	
+	public void clickNikonCamera()
+	{
+		NikonCamera.click();
+	}
+	
+	public void clickAddtoWish2()
+	{
+		AddtoWish2.click();
+	}
+	
+	public void clickTablets()
+	{
+		Tablets.click();
+	}
+	
+	public void clickAddtoWish3()
+	{
+		AddtoWish3.click();
+	}
+	
+	public void clickWishlistmenu()
+	{
+		Wishlistmenu.click();
+	}
+	
+	public String getConfirmationMsg()
+	{
+		try {
+			return(txtConfirm.getText());
+		}
+		catch(Exception e)
+		{
+			return (e.getMessage());
+		}
+	}
+	
+	public boolean isSuccessMessageDisplayed() {
+	    try {
+	        return alert.isDisplayed();
+	    } catch (Exception e) {
+	        return false;
+	    }
+	}
+	
+	public void clickcontin()
+	{
+		contin.click();
+	}
+	
+	
+	public void clickAccoWL()
+	{
+		AccoWL.click();
+	}
+}
---
+++ src/test/java/testCases/TC007_AddtoWishlist.java
@@ -0,0 +1,79 @@
+package testCases;
+
+import org.openqa.selenium.JavascriptExecutor;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import pageObjects.AddToWishlistPage;
+import pageObjects.LoginPage;
+import testBase.BaseClass;
+
+public class TC007_AddtoWishlist extends BaseClass {
+	
+	@Test
+	public void verifyWishlist()
+	{
+		
+		try
+		{
+			logger.info("Wishlist test started........");
+			AddToWishlistPage wp = new AddToWishlistPage(driver);
+			wp.clickCamera();
+			
+			JavascriptExecutor js = (JavascriptExecutor) driver;
+			js.executeScript("window.scrollBy(0,300)", "");
+			System.out.println(js.executeScript("return window.pageYOffset;"));
+			
+			wp.clickAddtoWish();
+			
+			wp.clickNikonCamera();
+			wp.clickAddtoWish2();
+			
+			driver.navigate().back();
+			
+			wp.clickTablets();
+			
+			wp.clickAddtoWish3();
+			
+			
+			Assert.assertTrue(wp.isSuccessMessageDisplayed(), "Success message not displayed!");
+			
+			wp.clickWishlistmenu();
+			
+			LoginPage lp = new LoginPage(driver);
+			lp.Username(p.getProperty("email"));
+			
+			lp.Passwrod(p.getProperty("password"));
+			
+			lp.clickLogin();
+			
+			
+			String confmsg = wp.getConfirmationMsg();
+			if(confmsg.equals("My Wish List"))
+			{
+				Assert.assertTrue(true);
+			}
+			else
+			{
+				logger.error("test failed........");
+				logger.debug("debug logss....");
+				Assert.assertTrue(false);
+			}
+			
+			wp.clickcontin();
+			
+			wp.clickAccoWL();
+			
+			 logger.info("Wishlist test ended........");
+		
+		}
+		catch(Exception e)
+		{
+			Assert.fail();
+		}
+		
+	}
+	
+	
+
+}
